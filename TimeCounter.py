10d6c6499a5662e83100c0a7c2e59ac3621eb888
Timothy Salisbury <salisbtimo@ecs.vuw.ac.nz>
2020-06-08 18:39:34 +1200

    Now include meeting time | Made output in MD

--- TimeCounter.py
+++ TimeCounter.py
@@ -1,65 +1,161 @@
+from datetime import datetime
+
 import gitlab
 import re
 from datetime import timedelta
 import configparser as cfg
 from os import path
 
-if not path.exists('config.ini'):
+file_dir = path.dirname(path.abspath(__file__))
+
+if not path.exists(f"{file_dir}/config.ini"):
     print("config.ini not found!")
     exit(1)
 
 config = cfg.ConfigParser()
-config.read('config.ini')
+config.read(f"{file_dir}/config.ini")
 
 
-def process_sprint(sprint, time_tracking):
-    print("***********" + sprint.title + "***********")
+def process_sprint(sprint, per_sprint_time_tracking, overall_time_tracking):
+    """
+    Processes a sprint object, first processes all issues found in it to calculate the time spent per user.
+
+    :param sprint:                      The sprint object to process
+    :param per_sprint_time_tracking:    The per_sprint_time_tracking dictionary, used to keep track of time per user per sprint
+    :param overall_time_tracking:       The overall time tracking, used to keep track of users overall time spent
+    :return:
+    """
     sprint_issues = sprint.issues(all=True)
-    print(str(len(sprint_issues)) + " issues found")
     user_time_track = {}
     for issue in sprint_issues:
         process_issue(issue, user_time_track)
-    print("Break down for " + sprint.title)
+    # Add the sprint to the per_sprint_time_tracking dictionary, where the value is the breakdown of time per user
+    if sprint not in per_sprint_time_tracking:
+        per_sprint_time_tracking[sprint] = user_time_track
+    # Add on the times spent by people to their overall time
     for key, value in user_time_track.items():
-        if key not in time_tracking:
-            time_tracking[key] = 0
-        time_tracking[key] += value
-        print('{}{}'.format(key.ljust(30), str(timedelta(minutes=int(value)))[:-3] + " hours"))
+        if key not in overall_time_tracking:
+            overall_time_tracking[key] = 0
+        overall_time_tracking[key] += value
 
 
-def process_issue(issue, user_time_track):
+def process_issue(issue, per_sprint_time_tracking):
+    """
+    Processes an issue
+    """
     discussions = issue.discussions.list(all=True)
     for discussion in discussions:
-        process_discussion(discussion, user_time_track)
+        process_discussion(discussion, per_sprint_time_tracking)
 
 
-def process_discussion(discussion, user_time_tracking):
+def process_discussion(discussion, per_sprint_time_tracking):
+    """
+    Processes a discussion, filters out everything except for time added notes.
+    """
     notes = list(filter(lambda _note: time_tracking_pattern.match(_note['body']), discussion.attributes['notes']))
     for note in notes:
-        process_note(note, user_time_tracking)
+        process_note(note, per_sprint_time_tracking)
 
 
-def process_note(note, user_time_tracking):
+def process_note(note, per_sprint_time_tracking):
+    """
+    Processes a note
+    """
     captures = re.search(time_tracking_pattern, note['body'])
     user = note['author']['name']
-    if user not in user_time_tracking:
-        user_time_tracking[user] = 0
+    # Check that user exists in the dictionary, if not then add them
+    if user not in per_sprint_time_tracking:
+        per_sprint_time_tracking[user] = 0
 
+    # Calculate amount of time added
     if 'm' in captures.group(2):
-        user_time_tracking[user] += int(captures.group(1))
+        per_sprint_time_tracking[user] += int(captures.group(1))
     else:
-        user_time_tracking[user] += int(captures.group(1)) * 60
+        per_sprint_time_tracking[user] += int(captures.group(1)) * 60
 
     if captures.group(4) is not None:
-        user_time_tracking[user] += int(captures.group(4))
+        per_sprint_time_tracking[user] += int(captures.group(4))
 
 
-def print_time_tracking(time_tracking):
-    for key, value in time_tracking.items():
-        print('{}{}'.format(key.ljust(30), str(timedelta(minutes=int(value)))[:-3] + " hours"))
+def print_time_tracking_per_sprint(per_sprint_time_tracking):
+    print("## Per Sprint Breakdown")
+    for sprint, time in per_sprint_time_tracking.items():
+        if len(time) == 0:
+            continue
+        print("###" + sprint.title)
+        print("| User | Time Spent  |")
+        print("|-----|---|")
+        for key, value in time.items():
+            print("|" + key + "|" + str(timedelta(minutes=int(value)))[:-3] + " hours" + "|")
+
+
+def print_overall_time_tracking(overall_time_tracking):
+    print("## Overall Breakdown")
+    print("| User | Time Spent  |")
+    print("|-----|---|")
+    for key, value in overall_time_tracking.items():
+        print("|" + key + "|" + str(timedelta(minutes=int(value)))[:-3] + " hours" + "|")
+
+
+def process_lab_session_times(group_lab_wiki, sprint_time_tracking, overall_time_tracking):
+    lab_tables = re.findall(group_lab_time_table_pattern, group_lab_wiki.content)
+    for table in lab_tables:
+        process_table(table, sprint_time_tracking, overall_time_tracking)
+
+
+def get_sprint_from_date(date):
+    for sprint in sprints:
+        start_date = sprint.attributes['start_date'].split('-')
+        start_date = (int(start_date[1]), int(start_date[2]))
+        end_date = sprint.attributes['due_date'].split('-')
+        end_date = (int(end_date[1]), int(end_date[2]))
+
+        # https://stackoverflow.com/questions/5464410/how-to-tell-if-a-date-is-between-two-other-dates-in-python/5464467
+        if start_date <= date <= end_date:
+            return sprint
+
+    return None
+
+
+def get_time_from_string(date_string):
+    try:
+        timestamp = datetime.strptime(date_string, '%I:%M%p')
+    except ValueError:
+        timestamp = datetime.strptime(date_string, '%I%p')
+    return timestamp
+
+
+def process_table(table, sprint_time_tracking, overall_time_tracking):
+    rows = re.findall(table_row_pattern, table)
+    for row in rows:
+        date = row[0].split('/')
+        date = (int(date[1]), int(date[0]))
+        sprint = get_sprint_from_date(date)
+        if sprint is not None:
+            # https://stackoverflow.com/questions/3096953/how-to-calculate-the-time-interval-between-two-time-strings
+            start_time = get_time_from_string(row[4])
+            end_time = get_time_from_string(row[8])
+            # Get total time spent, turn into seconds
+            minutes_spent = (end_time - start_time).seconds / 60
+            # Add time onto the tracking maps
+            for key, value in overall_time_tracking.items():
+                if row[3] in key.split(' ')[1]:
+                    add_entry(key, minutes_spent, overall_time_tracking, sprint_time_tracking[sprint])
+
+
+def add_entry(key, value, overall_time_tracking, per_sprint_time_tracking):
+    if key not in overall_time_tracking:
+        overall_time_tracking[key] = 0
+    if key not in per_sprint_time_tracking:
+        per_sprint_time_tracking[key] = 0
+    overall_time_tracking[key] += value
+    per_sprint_time_tracking[key] += value
 
 
 time_tracking_pattern = re.compile('added (\d+)([h|m])( (\d+)m)?')
+group_lab_time_table_pattern = re.compile('#{4} Group Lab Session \d+(?:.+)?(?:\n\|(?:[^|]+\|){5})+')
+table_row_pattern = re.compile(
+    '\|\s+(\d+\/\d+\/\d+)\s+\|\s*\w+\s*\|\s*((\w+) (\w+))\s*\|\s*((\d+(.\d+)?)(pm|am))\s*\|\s*((\d+(.\d+)?)(pm|am))\s*\|')
 
 gl = gitlab.Gitlab(config['AUTHENTICATION']['GitLabServer'],
                    private_token=config['AUTHENTICATION']['PersonalAccessToken'])
@@ -68,13 +164,18 @@ project = gl.projects.get(config['PROJECT']['ID'])
 
 milestones = project.milestones.list()
 
-time_tracking = {}
+per_sprint_time_tracking = {}
+over_all_time_tracking = {}
 
 sprints = list(filter(lambda milestone: config['PROJECT']['SprintMilestonePrefix'] in milestone.title, milestones))
 
 for sprint in sprints:
-    process_sprint(sprint, time_tracking)
+    process_sprint(sprint, per_sprint_time_tracking, over_all_time_tracking)
 
-print("\n")
-print("***********Overall Break Down***********")
-print_time_tracking(time_tracking)
+if config['PROJECT']['LabTimeWikiSlug'] is not None:
+    process_lab_session_times(project.wikis.get(config['PROJECT']['LabTimeWikiSlug']), per_sprint_time_tracking,
+                              over_all_time_tracking)
+
+print("# Break down for time spent per person")
+print_time_tracking_per_sprint(per_sprint_time_tracking)
+print_overall_time_tracking(over_all_time_tracking)
